4eb99e0fe1835358832f56033c42fe275ca0ff90
afonsof3rreira <afonsof3rreira@users.noreply.github.com>
2021-06-15 02:18:30 +0100

    -included real-time seizure detection

--- data_processing/python_code/rt_data_reader_USB.py
+++ data_processing/python_code/rt_data_reader_USB.py
@@ -34,7 +34,7 @@ def main(save_data: bool, detect_seizure: bool, result_dir: str, serial_baud_r: 
                window_overlap (int): the percentage of overlap between successive windows.
     """
 
-    global arduino, first_ft_window, temp_window_data, window_overlap_frac, mag_list, abs_fft_list, times_fft
+    global arduino, first_ft_window, temp_window_data, window_overlap_frac, mag_list, abs_fft_vm_list, times_fft, abs_fft_sma_list, detection_window_it, detection_window, scr_vm, scr_sma, scr_vm_sum, scr_sma_sum, vm_weights, sma_weights, SMA_thrs, VM_thrs
     default, port = check_default_port(serial_port)  # the Port name as a string
 
     """Initialising Serial connection"""
@@ -73,12 +73,35 @@ def main(save_data: bool, detect_seizure: bool, result_dir: str, serial_baud_r: 
     interval_mark = 1
 
     if detect_seizure:
+        threshold_param_file = "experiment_a.csv"
+        path_to_thresholds = os.path.join(os.path.dirname(sys.argv[0]), "classification_results", threshold_param_file)
+        path_to_spectral_weights = os.path.join(os.path.dirname(sys.argv[0]), "data", "results", "2021-06-12-22-50-19", "a", "spectral_weights_a.csv")
+
+        df_thresholds = pd.read_csv(path_to_thresholds, usecols=['VM thrs.', 'SMA thrs.'])
+        max_params = df_thresholds.iloc[[-1]]
+
+        df_spectral_weights = pd.read_csv(path_to_spectral_weights, index_col=[0])
+        vm_weights = np.transpose(df_spectral_weights['VM weights'].to_numpy())
+        sma_weights = np.transpose(df_spectral_weights['SMA weights'].to_numpy())
+        # print(vm_weights)
+
+        VM_thrs = 1.6886873621458105    # max_params.iloc[0]['VM thrs.'] ,
+        SMA_thrs = 1.6174331964472095     # max_params.iloc[0]['SMA thrs.']
+        print(VM_thrs)
+
         first_ft_window = True
         temp_window_data = collections.deque([first_read_line])
         window_overlap_frac = 100 // window_overlap
         mag_list = []
-        abs_fft_list = []
+        abs_fft_vm_list = []
+        abs_fft_sma_list = []
         times_fft = []
+        detection_window = 5  # seconds
+        detection_window_it = 0
+        scr_vm_sum = 0
+        scr_sma_sum = 0
+        scr_vm = 0
+        scr_sma = 0
 
     # recording data until the recording_time stopping criterion is met
     while time_curr - time_i < recording_time * 1000 * 60:  # min to ms
@@ -105,31 +128,100 @@ def main(save_data: bool, detect_seizure: bool, result_dir: str, serial_baud_r: 
                 # algorithm itself
 
                 # TODO: until here, we have (sf * ft_window_s) + 1 points = ft_window_s seconds.
-                mag_arr, abs_ft_arr = detection_algorithm(temp_window_data)
+                abs_ft_vm_arr, abs_ft_sma_arr = detection_algorithm(temp_window_data)
                 times_fft.append(time_curr - time_i)
-                mag_list.append(mag_arr)
-                abs_fft_list.append(abs_ft_arr)
+                abs_fft_vm_list.append(abs_ft_vm_arr)
+                abs_fft_sma_list.append(abs_ft_sma_arr)
+                # print(abs_ft_vm_arr.shape)
+                # print(vm_weights.shape)
+
+                vm_num = np.squeeze(np.multiply(abs_ft_vm_arr, vm_weights))
+                sma_num = np.squeeze(np.multiply(abs_ft_sma_arr, sma_weights))
+
+                scr_vm = np.divide(np.sum(vm_num, axis=0), np.sum(np.squeeze(abs_ft_vm_arr), axis=0))
+                scr_sma = np.divide(np.sum(sma_num, axis=0), np.sum(np.squeeze(abs_ft_sma_arr), axis=0))
 
                 # remove the first/left (sf * ft_window_s) / 2 points, resulting in (sf * ft_window_s) + 1 points
                 for _ in range(sf * ft_window_s // window_overlap_frac):
                     temp_window_data.popleft()
+
                 first_ft_window = False
+                # print('first')
+                # print(scr_vm)
+
+                detection_window_it += 1
+                scr_vm_sum += scr_vm
+                scr_sma_sum += scr_sma
 
                 # np.itertools.starmap(temp_window_data.popleft, np.repeat((), 4096))
 
             # if the next overlapping window was recorded, start detection thread and update the temp. window
             elif ((time_curr - time_i) * 0.001) % (ft_window_s / window_overlap_frac) == 0 and not first_ft_window:
+
+
+
                 times_fft.append(time_curr - time_i)
-                mag_arr, abs_ft_arr = detection_algorithm(temp_window_data)
-                mag_list.append(mag_arr)
-                abs_fft_list.append(abs_ft_arr)
+                abs_ft_vm_arr, abs_ft_sma_arr = detection_algorithm(temp_window_data)
+                abs_fft_vm_list.append(abs_ft_vm_arr)
+                abs_fft_sma_list.append(abs_ft_sma_arr)
+
+                vm_num = np.squeeze(np.multiply(abs_ft_vm_arr, vm_weights))
+                # print(vm_num)
+                sma_num = np.squeeze(np.multiply(abs_ft_sma_arr, sma_weights))
+
+
+                scr_vm = np.divide(np.sum(vm_num, axis=0), np.sum(np.squeeze(abs_ft_vm_arr), axis=0))
+                scr_sma = np.divide(np.sum(sma_num, axis=0), np.sum(np.squeeze(abs_ft_sma_arr), axis=0))
+                # print(scr_vm)
+                # print('--')
+                # print(scr_vm)
+                detection_window_it += 1
+                # print('next')
+                #
+                # print(scr_vm)
+                scr_vm_sum += scr_vm
+                scr_sma_sum += scr_sma
 
                 # remove the first/left (sf * ft_window_s) / 2 points, resulting in (sf * ft_window_s) + 1 points
                 for _ in range(sf * ft_window_s // window_overlap_frac):
                     temp_window_data.popleft()
 
+
             # print(' Time elapsed:', timeit.default_timer() - start_time, 's')
-        # TODO: if detection serial.write(beep)
+
+        # print(detection_window_it)
+
+        if ((time_curr - time_i) * 0.001) % detection_window == 0:
+            # print('finish')
+
+            scr_vm_avg = scr_vm_sum / (detection_window_it + 1)
+            scr_sma_avg = scr_sma_sum / (detection_window_it + 1)
+            # print(scr_vm_avg)
+
+            # print(scr_vm_avg, detection_window_it + 1)
+            # print(scr_sma_avg, detection_window_it + 1)
+            # print('--')
+            # print()
+
+            # print()
+            # print(scr_vm_avg)
+            # print(VM_thrs)
+
+            if scr_vm_avg > VM_thrs and scr_sma_avg > SMA_thrs:
+                arduino.write(b's')
+                print('!!!!!!!!!!!!')
+
+            else:
+                arduino.write(b'n')
+
+            scr_vm_sum = 0
+            scr_sma_sum = 0
+            scr_vm_avg = 0
+            scr_sma_avg = 0
+
+            detection_window_it = 0
+
+
 
     print("...transmission finished")
 
@@ -164,13 +256,12 @@ def detection_algorithm(data_chunk: collections.deque, magnitude=True):
     # check this one too https://odr.chalmers.se/bitstream/20.500.12380/145997/1/145997.pdf
 
     # TODO: compute the FFT weights based on a series of seizure vs baseline datasets [1]
-    mag_arr = getArrayfromStringList(data_chunk, magnitude=magnitude)
+    vm_arr, sma_arr = getArrayfromStringList(data_chunk)
 
-    ft_arr = np.fft.rfft(mag_arr)
-    abs_ft_arr = np.abs(ft_arr)
-    power_ft_arr = np.square(ft_arr)  # redundant, if every ft is expressed as absolute
-    # frequency = np.linspace(0, sampling_rate/2, len(power_spectrum))
-    return mag_arr, abs_ft_arr
+    abs_ft_vm_arr = np.abs(np.fft.rfft(vm_arr))
+    abs_ft_sma_arr = np.abs(np.fft.rfft(sma_arr))
+
+    return abs_ft_vm_arr, abs_ft_sma_arr
 
 
 def write_2(times_fft_l, mag_l, abs_fft_l):
@@ -212,24 +303,17 @@ def write_2(times_fft_l, mag_l, abs_fft_l):
     return newl, header
 
 
-def getArrayfromStringList(data_lines: collections.deque, magnitude: bool):
-    if magnitude:
-        data_arr = np.zeros((1, len(data_lines)))  # [mag_acc_t1, mag_acc_t2, ..., mag_acc_tn]
-        for i in range(len(data_lines)):
-            temp = data_lines[i].replace(" ", "")
-            temp = temp[2:-5].split(',')
-            data_arr[0, i] = np.linalg.norm(np.array([float(temp[1]), float(temp[2]), float(temp[3])]))
+def getArrayfromStringList(data_lines: collections.deque):
 
-    else:
-        data_arr = np.zeros((3, len(data_lines)))
-        for i in range(len(data_lines)):
-            temp = data_lines[i].replace(" ", "")
-            temp = temp[2:-5].split(',')
-            data_arr[0, i] = float(temp[1])
-            data_arr[1, i] = float(temp[2])
-            data_arr[2, i] = float(temp[3])
+    data_arr_vm = np.zeros((1, len(data_lines)))  # [mag_acc_t1, mag_acc_t2, ..., mag_acc_tn]
+    data_arr_sma = np.zeros((1, len(data_lines)))
+    for i in range(len(data_lines)):
+        temp = data_lines[i].replace(" ", "")
+        temp = temp[2:-5].split(',')
+        data_arr_vm[0, i] = np.linalg.norm(np.array([float(temp[1]), float(temp[2]), float(temp[3])]))
+        data_arr_sma[0, i] = (abs(float(temp[1])) + abs(float(temp[2])) + abs(float(temp[3]))) / 3
 
-    return data_arr
+    return data_arr_vm, data_arr_sma
 
 def check_default_port(port_name):
     port_str = port_name
@@ -264,7 +348,7 @@ if __name__ == "__main__":
     parser.add_argument(
         '--detect_seizure',
         type=bool,
-        default=False,
+        default=True,
         help='Set to True to enable real-time seizure detection'
     )
 
